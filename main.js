abc492bcf2c3b6000ae95d803f005efe01676bce
xiaomeng2372 <changhao1997@unc.edu>
2018-12-03 22:35:02 -0500

    - Add main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+var root = "http://comp426.cs.unc.edu:3001/";
+
+/*
+$(document).ready(()=>{
+  let userName = changhao;
+  let pw = 730086729;
+  $.ajax(root+'sessions',{
+  type:'POST',
+  xhrFields: {withCredentials: true},
+  data:{
+    user:{
+    username:userName,
+    password:pw
+  }
+  },
+  success:(r)=>{
+    if(r.status){
+      alert('Login Success!')
+      $('#msg').html("Success!");
+       showMe();
+    }else{
+      $('#msg').html("It seems that your password is wrong. Please try again");
+    }
+  },
+  error: () => {
+      alert('XMLHttpRequest failed. Please check your network');
+      //$('#msg').append('<button'+'id=button_'+allQuestions[i]+'>'submit'</button>');
+  }
+
+
+  });
+*/
