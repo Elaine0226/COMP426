dbba8de71367c98ee1e5f6f34ae11523073d7082
Elaine0226 <yizhongz@live.unc.edu>
2018-12-05 12:09:02 -0500

    Add files via upload

--- bookticket.js
+++ bookticket.js
@@ -1,20 +1,33 @@
 var root = "http://comp426.cs.unc.edu:3001/";
 var root_url = "http://comp426.cs.unc.edu:3001/";
 
-//clean all build_purchase_interface
+//clean all after click book button, build_purchase_interface
 
 $(document).ready(function () {
-    $(".Search").click(function () {
+    $(".search").click(function () {
         build_purchase_interface();
     });
 });
 
+// go to my trip page
+$(document).ready(function () {
+    $(".mytrips").click(function () {
+        build_mytrip_interface();
+    });
+});
+
+
+
 
 var build_purchase_interface = function () {
+    let nav = $('<div class="nav_div"> </div>');
+    nav.append('<button class = "home"> Home </button>')
+    nav.append('<button class="myTrips">My Trips</button>')
+    nav.append('<button class="log">Log In</button>')
+
+
     let body = $('body');
     body.empty();
-
-
 
     let firstName = $('<div class="firstName">First Name: </div>');
     firstName.append('<input type="text" name="firstName" id="firstName" placeholder="Put your first name" required></br>');
@@ -27,7 +40,7 @@ var build_purchase_interface = function () {
     lastName.append('<input type="text" name="lastName" id="lastName" placeholder="Put your last name" required></br>');
 
     let age = $('<div class="age">Age: </div>');
-    age.append('<input type="text" name="numeric" id = "age" class="numberonly"placeholder = "Your age" required>');
+    age.append('<input type="text" name="numeric" id = "age" class="numberonly" placeholder = "Your age" required>');
 
     let gender = $('<div class="gender">Gender: </div>');
     gender.append('<label><input type="radio" name="gender" value="male" required> Male</label> ')
@@ -39,42 +52,61 @@ var build_purchase_interface = function () {
     let seatID = $('<div class="seatID">Seat ID: </div>');
     seatID.append('<input type="text" name="seatID" id="seatID" class = "numberonly" placeholder="Put your seat ID"></br>')
 
-
     let purchaseBtn = $('<input type="button" name="purchase" class = "unpurchased" value="Purchase"></input>');
 
 
-    let dataticket = {
-        "ticket": {
-            "first_name": $("#firstName").val(),
-            "middle_name": $("#middleName").val(),
-            "last_name": $("#lastName").val(),
-            "age": $("#age").val(),
-            "gender": $("input[name='gender']:checked").val(),
-            "is_purchased": true,
-            //"price_paid": "290.11",
-            "instance_id": $("#instanceID").val(),
-            "seat_id": $("#seatID").val(),
-        }
-    }
+
+    // let dataticket = {};
+    // let ticketData = {};
+    // ticketData["first_name"] = $("#firstName").val();
+    // dataticket["ticket"] = ticketData;
+
     $(purchaseBtn).click(function () {
+
         $(this).removeClass('unpurchased').addClass('purchased');
-        $.ajax(root_url + 'tickets',
-            {
-                type: 'POST',
-                xhrFields: { withCredentials: true },
-                data: dataticket,
-                success: function (buildMyTripPage) {
-                    alert("Purchased!");
+
+        console.log({
+            "ticket": {
+                "first_name": $("#firstName").val(),
+                "middle_name": $("#middleName").val(),
+                "last_name": $("#lastName").val(),
+                "age": parseInt($("#age").val(), 10),
+                "gender": $("input[name='gender']:checked").val(),
+                "is_purchased": true,
+                //"price_paid": "290.11",
+                "instance_id": parseInt($("#instanceID").val(), 10),
+                "seat_id": parseInt($("#seatID").val(), 10),
+
+            }
+        }
+
+
+        );
+        $.ajax(root_url + 'tickets', {
+            type: 'POST',
+            xhrFields: { withCredentials: true },
+            data: {
+                "ticket": {
+                    "first_name": $("#firstName").val(),
+                    "middle_name": $("#middleName").val(),
+                    "last_name": $("#lastName").val(),
+                    "age": parseInt($("#age").val(), 10),
+                    "gender": $("input[name='gender']:checked").val(),
+                    "is_purchased": true,
+                    "instance_id": parseInt($("#instanceID").val(), 10),
+                    "seat_id": parseInt($("#seatID").val(), 10),
                 }
+            },
+            success: function (build_mytrip_interface) {
+                alert("Purchased!");
+            }
 
 
-            });
+        });
 
     });
 
-    var buildMyTripPage = function () {
-        alert("buildmytrip");
-    }
+
 
     //numberonly not working
     $(".numberonly").on("keypress keyup blur", function (event) {
@@ -88,6 +120,7 @@ var build_purchase_interface = function () {
 
 
     $('body')
+        .append(nav)
         .append('<div>search result</div')//show search result
 
         .append('<form id="myInfo"></form>');
@@ -102,5 +135,40 @@ var build_purchase_interface = function () {
         .append(instanceID)
         .append(seatID)
         .append(purchaseBtn)
+
+}
+//my trip page
+var build_mytrip_interface = function () {
+    alert("buildmytrip");
+    let nav = $('<div class="nav_div"> </div>');
+    nav.append('<button class = "home"> Home </button>')
+    nav.append('<button class="myTrips">My Trips</button>')
+    nav.append('<button class="log">Log In</button>')
+
+    let body = $('body');
+    body.empty();
+
+    let mytickets = $('<div class="mytickets">My tickets: </div>');
+
+    $.ajax(root_url + 'tickets',
+        {
+            type: 'GET',
+            xhrFields: { withCredentials: true },
+            data: dataticket,
+            success: function (build_mytrip_interface) {
+                alert("Purchased!");
+            }
+
+
+        });
+
+
+
+
+
+
+
+    $('body')
+        .append(nav);
 
 }
