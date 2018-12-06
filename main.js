9ba0d72b0cc9fd47bb5201f60a8545ec95f30c34
xiaomeng2372 <changhao1997@unc.edu>
2018-12-06 17:27:17 -0500

    - Fixed some bugs and added flight info

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var root = "http://comp426.cs.unc.edu:3001/";
 var root_url = "http://comp426.cs.unc.edu:3001/";
 var departure, destination, date_go, date_back;
 var selected, selected_go, selected_back; //variables for recording selected flights_id onchange of input radio
-
+var planeResult;
 //use input text to search corresponding departure and arrival airport
 var search_flight = function() {
   departure = $("#departure").val();            //get values of each input text
@@ -215,8 +215,24 @@ function returnFlight(departure_array, arrival_array){
                 time += " - ";
                 time += response[count].arrives_at.substring(11, 16);
                 find_airline(response[count].airline_id, k, time, "#general_back_", destination, departure);
+
+                $.ajax(root_url + "planes/" + response[count].plane_id, {
+                  type: "GET",
+                  xhrFields: {withCredentials: true},
+                  success: (r) =>{
+                    //alert(r);
+                    planeResult = r.name;
+                    //alert(result);
+                   //alert(planeResult);
+                  },
+                  error: ()=>{
+                    alert("Can't find such plane info");
+                    //$(cname+count).remove();
+                  }
+                });
                 addDetails(response[count],'#result_back_'+k,k,destination,departure);
                 k++;
+
               }
               $(".errormsg").html("");
             }
@@ -243,25 +259,49 @@ function returnFlight(departure_array, arrival_array){
    let flightNumber = 'Flight Number: ' + response.number;
    let depart1 = 'Departure Airport: ' + depart;
    let destin = 'Destination Airport: '+ arrival;
-
    let departHour = parseInt(response.departs_at.substring(11,13))*60;
    let departMin = parseInt(response.departs_at.substring(14,16))
    let t1 = departHour + departMin;
    let arriveHour = parseInt(response.arrives_at.substring(11,13))*60;
    let arriveMin = parseInt(response.arrives_at.substring(14,16))
    let t2 = arriveHour + arriveMin;
-   let timeLast = 'Time on the air: '+(t2 - t1)/60.00 + 'Hours';
+   let t3 = ((t2 -t1)/60.00);
+
+
+   //alert(planeId);
+   // if it's a negative number, add 24 hours
+   if (t3<0){
+     //alert(t3);
+     t3 = t3 + 24.00;
+     //alert(t3)
+     t3.toFixed(2);
+   }
+
+   t3 = t3.toFixed(2);
+   let planeId = response.plane_id;
+
+
+   //alert('wow ' + planeId);
+   //findPlaneInfo(planeId);
+   //alert(planeResult);
+   //let planeInfo = 'Plane name: '+ planeResult;
+   //alert('wow' + findPlaneInfo(planeId));
+   let timeLast = 'Time on the air: '+ t3 + ' Hours';
 
    let detailDiv = $('<div class="details" id="details_'+k+'" ></div>')
    $(id).append(detailDiv);
-
+   //findPlaneInfo(planeId,'details_'+k);
    let departTimeDiv = $('<div>'+departTime+'</div>');
    let arrivalTimeDiv = $('<div>'+arrTime+'</div>');
    let flightNumberDiv = $('<div>'+flightNumber+'</div>');
    let departAirDiv = $('<div>'+depart1+'</div>');
    let destinAirDiv = $('<div>'+destin+'</div>');
    let timeLastDiv = $('<div>'+timeLast+'</div>');
+   //let planeDiv = $('<div>'+ haha +'</div>');
 
+   findPlaneInfo(planeId,'#details_'+k);
+
+   //detailDiv.append(planeDiv);
    detailDiv.append(departTimeDiv);
    detailDiv.append(arrivalTimeDiv);
    detailDiv.append(flightNumberDiv);
@@ -294,4 +334,26 @@ function returnFlight(departure_array, arrival_array){
  this.selectFlight_back = function(flight_id){
    selected_back = flight_id;
    console.log(flight_id);
+ }
+
+ function findPlaneInfo(planeID,id){
+   $.ajax(root_url + "planes/" + planeID, {
+     type: "GET",
+     xhrFields: {withCredentials: true},
+     success: (r) =>{
+       //alert(r);
+       //when(r.name)
+       planeResult = r.name;
+       //alert(planeResult);
+       $(id).append('<div>Plane: '+planeResult+'</div> ');
+
+       //alert('jerry')
+       //alert(result);
+      //alert(planeResult);
+     },
+     error: ()=>{
+       alert("Can't find such plane info");
+       //$(cname+count).remove();
+     }
+   });
  }
