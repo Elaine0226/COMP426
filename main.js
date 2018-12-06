e05160b61e364965c855dd4c43094a1686210053
xiaomeng2372 <changhao1997@unc.edu>
2018-12-05 21:48:17 -0500

    - Add details for oneway but still need roundtrip

--- main.js
+++ main.js
@@ -65,12 +65,12 @@ var result_page = function(departure_array, arrival_array, departure, destinatio
           //for each response, build its own div with specific information
           //consider if the trip is oneway or roundtrip, building different interfaces for each
           if (oneWay){
-            if (response.length!=0){
+            if (response.length!=0){ //flight info
               for (var count = 0; count < response.length; count++){
                 $(".results").append("<div class = 'result' id = 'result_" + k + "' ></div>");
-                let time = response[count].departs_at.substring(11, 16);
 
-                // add select buttons
+
+
                 id = '#result_'+k;
                 if(k==1){
                   $(id).append("<input type='radio' id='oneWay_go_"+k+"' class='oneWay_go_' name='oneWay_go_button' checked>")
@@ -78,11 +78,13 @@ var result_page = function(departure_array, arrival_array, departure, destinatio
                   $(id).append("<input type='radio' id='oneWay_go_"+k+"' class='oneWay_go_' name='oneWay_go_button'>")
                 }
 
-
+                let time = response[count].departs_at.substring(11, 16);
                 time += " - ";
                 time += response[count].arrives_at.substring(11, 16);
                 find_airline(response[count].airline_id, k, time, "#result_", departure, destination);
+                addDetails(response[count],'#result_'+k,k,departure,destination);
                 k++;
+
               }
               $(".errormsg").html("");
             }
@@ -135,6 +137,7 @@ var find_airline = function(id, count, time, cname, depart, arrive){
       $(cname + count).append("<div class = 'flight_airline' id = 'flight_airline_" + count + "' >" + result + "</div>");
       $(cname + count).append("<div class = 'flight_time' id = 'flight_time_" + count + "' >" + time + "</div>");
       $(cname + count).append("<div class = 'flight_places id = 'flight_places_" + count + "' >" + depart + " -> " + arrive + "</div>");
+      $(cname + count).append("<button onclick='showDetails(e)'>Details</button>")
     },
     error: ()=>{
       $(cname+count).remove();
@@ -200,10 +203,6 @@ function returnFlight(departure_array, arrival_array){
 
 
 
-                // Select, details button
-                // roundtrip 两个 保证有两个都选上
-                // detail button
-
                 let time = response[count].departs_at.substring(11, 16);
                 time += " - ";
                 time += response[count].arrives_at.substring(11, 16);
@@ -225,3 +224,61 @@ function returnFlight(departure_array, arrival_array){
   }
 
 }
+
+function diff(start, end) {
+    start = start.split(":");
+    end = end.split(":");
+    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    var diff = endDate.getTime() - startDate.getTime();
+    var hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(diff / 1000 / 60);
+
+    // If using time pickers with 24 hours format, add the below line get exact hours
+    if (hours < 0)
+       hours = hours + 24;
+
+    return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
+}
+
+function findCompany(id){
+
+}
+
+ function addDetails(response,id,k,depart, arrival){
+   let time = response.departs_at.substring(11, 16);
+   // response[count] single flight
+   // add select buttons
+   let departTime = 'Departure Time: '+ response.departs_at.substring(11,16);
+   let arrTime = 'Arrival Time: '+ response.arrives_at.substring(11,16);
+   let flightNumber = 'Flight Number: ' + response.number;
+   let depart1 = 'Departure Airport: ' + depart;
+   let destin = 'Destination Airport: '+ arrival;
+
+   let departHour = parseInt(response.departs_at.substring(11,13))*60;
+   let departMin = parseInt(response.departs_at.substring(14,16))
+   let t1 = departHour + departMin;
+   let arriveHour = parseInt(response.arrives_at.substring(11,13))*60;
+   let arriveMin = parseInt(response.arrives_at.substring(14,16))
+   let t2 = arriveHour + arriveMin;
+   let timeLast = 'Time on the air: '+(t2 - t1)/60.00 + 'Hours';
+
+   let detailDiv = $('<div class="details" id="details_'+k+'" ></div>')
+   $(id).append(detailDiv);
+
+   let departTimeDiv = $('<div>'+departTime+'</div>');
+   let arrivalTimeDiv = $('<div>'+arrTime+'</div>');
+   let flightNumberDiv = $('<div>'+flightNumber+'</div>');
+   let departAirDiv = $('<div>'+depart1+'</div>');
+    let destinAirDiv = $('<div>'+destin+'</div>');
+     let timeLastDiv = $('<div>'+timeLast+'</div>');
+
+     detailDiv.append(departTimeDiv);
+     detailDiv.append(arrivalTimeDiv);
+     detailDiv.append(flightNumberDiv);
+     detailDiv.append(departAirDiv);
+     detailDiv.append(destinAirDiv);
+     detailDiv.append(timeLastDiv);
+
+ }
