a29aa0f60ecd46e6e12bc9b404a4902bb577d7ab
xiaomeng2372 <changhao1997@unc.edu>
2018-12-08 22:05:15 -0500

    - Added autocomplete js file

---
+++ autoComplete.js
@@ -0,0 +1,55 @@
+function convertAirport(airport){
+  let data;
+  $.ajax(root_url + '/airports?filter[name]='+airport,{
+    type:'GET',
+    xhrFields: {withCredentials: true},
+    async: false,
+    success:(response)=>{
+     data = response;
+     console.log(data[0].code);
+     //return data[0].code;
+  }
+})
+return data[0].code;
+}
+
+   let airport = [];
+   //let airportCode = [];
+   $.ajax(root_url + 'airports',{
+     type:'GET',
+     xhrFields: {withCredentials: true},
+     success:(response)=>{
+
+       //console.log(data);
+
+       let data = response;
+       //console.log(data);
+       for(let i = 0;i<data.length;i++){
+         if(!airport.includes(data[i].name)){
+         airport.push(data[i].name);}
+
+       }
+
+       $("#departure").autocomplete({
+        source: function(request, response) {
+        var results = $.ui.autocomplete.filter(airport, request.term);
+
+        response(results.slice(0, 7));
+    }
+});
+       $("#destination").autocomplete({
+       source: function(request, response) {
+       var results = $.ui.autocomplete.filter(airport, request.term);
+
+       response(results.slice(0, 7));
+}
+});
+
+       //$('#departure').autocomplete( maxResults: 5,{source:airport});
+
+       //$('#departure').autocomplete( maxResults: 5,{source:airport});
+      //$('#destination').autocomplete( maxResults: 5,{source:airport});
+
+     }
+
+   });
