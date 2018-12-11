f8e903e8444e4706fff30b51dc9b61e0db4311b6
Yue Pi <42617039+SherryPi@users.noreply.github.com>
2018-12-11 10:50:46 -0500

    update

--- autoComplete.js
+++ autoComplete.js
@@ -1,59 +1,59 @@
-var root_url = "http://comp426.cs.unc.edu:3001/";
-
-
-function convertAirport(airport) {
+root_url = "http://comp426.cs.unc.edu:3001/";
+function convertAirport(airport){
   let data;
-  $.ajax(root_url + 'airports?filter[name]=' + airport, {
-    type: 'GET',
-    xhrFields: { withCredentials: true },
+  $.ajax(root_url + 'airports?filter[name]='+airport,{
+    type:'GET',
+    xhrFields: {withCredentials: true},
     async: false,
-    success: (response) => {
-      data = response;
-      console.log(data[0].code);
-      //return data[0].code;
+    success:(response)=>{
+     data = response;
+     //return data[0].code;
     }
   })
-  return data[0].code;
+  if (data.length == 0){
+    return airport.toUpperCase();
+  } else{
+    return data[0].code;
+  }
 }
 
-let airport = [];
-//let airportCode = [];
-$.ajax(root_url + 'airports', {
-  type: 'GET',
-  xhrFields: { withCredentials: true },
-  success: (response) => {
+   let airport = [];
+   //let airportCode = [];
+   $.ajax(root_url + 'airports',{
+     type:'GET',
+     xhrFields: {withCredentials: true},
+     success:(response)=>{
 
-    //console.log(data);
+       //console.log(data);
 
-    let data = response;
-    //console.log(data);
-    for (let i = 0; i < data.length; i++) {
-      if (!airport.includes(data[i].name)) {
-        airport.push(data[i].name);
-      }
+       let data = response;
+       //console.log(data);
+       for(let i = 0;i<data.length;i++){
+         if(!airport.includes(data[i].name)){
+         airport.push(data[i].name);}
 
+       }
+
+       $("#departure").autocomplete({
+        source: function(request, response) {
+        var results = $.ui.autocomplete.filter(airport, request.term);
+
+        response(results.slice(0, 7));
     }
-
-    $("#departure").autocomplete({
-      source: function (request, response) {
-        var results = $.ui.autocomplete.filter(airport, request.term);
-
-        response(results.slice(0, 7));
-      }
-    });
-    $("#destination").autocomplete({
-      source: function (request, response) {
-        var results = $.ui.autocomplete.filter(airport, request.term);
-
-        response(results.slice(0, 7));
-      }
-    });
-
-    //$('#departure').autocomplete( maxResults: 5,{source:airport});
-
-    //$('#departure').autocomplete( maxResults: 5,{source:airport});
-    //$('#destination').autocomplete( maxResults: 5,{source:airport});
-
-  }
-
 });
+       $("#destination").autocomplete({
+       source: function(request, response) {
+       var results = $.ui.autocomplete.filter(airport, request.term);
+
+       response(results.slice(0, 7));
+}
+});
+
+       //$('#departure').autocomplete( maxResults: 5,{source:airport});
+
+       //$('#departure').autocomplete( maxResults: 5,{source:airport});
+      //$('#destination').autocomplete( maxResults: 5,{source:airport});
+
+     }
+
+   });
